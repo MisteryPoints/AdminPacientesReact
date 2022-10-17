0a07f03d7ad8147f4f48ca20137ef50ba43b7f36
MisteryPoints <misterypoint2@gmail.com>
2022-10-17 18:57:37 -0400

    Final

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,26 +1,27 @@
 import Paciente from "./Paciente"
 
 const ListadoPacientes = ({pacientes, setPaciente, eliminarPaciente}) => {  
-    return(
-        <div className="md:w-1/2 lg:w-3/5 h-screen md:overflow-y-scroll md:scrollbar-thin  scrollbar-thumb-indigo-500 scrollbar-track-indigo-300">
-
+    return( 
+        <div div className="md:w-1/2 lg:w-3/5 h-screen">
             {pacientes && pacientes.length ? (
                 <>
-                    <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
-                    <p className="text-xl mt-5 mb-10 text-center">
-                        Aministra tus {''}
-                        <span className="text-indigo-600 font-bold">Pacientes y Citas
-                        </span>
-                    </p>
+                    <div className="md:overflow-y-scroll md:scrollbar-thin  scrollbar-thumb-indigo-500 scrollbar-track-indigo-300">
+                        <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+                        <p className="text-xl mt-5 mb-10 text-center">
+                            Aministra tus {''}
+                            <span className="text-indigo-600 font-bold">Pacientes y Citas
+                            </span>
+                        </p>
 
-                    {pacientes.map( paciente => 
-                    <Paciente 
-                        key= {paciente.id}
-                        paciente= {paciente}
-                        setPaciente= {setPaciente}
-                        eliminarPaciente= {eliminarPaciente}
-                    /> 
-                    )} 
+                        {pacientes.map( paciente => 
+                        <Paciente 
+                            key= {paciente.id}
+                            paciente= {paciente}
+                            setPaciente= {setPaciente}
+                            eliminarPaciente= {eliminarPaciente}
+                        /> 
+                        )} 
+                    </div>
                 </>
             ) : (
                 <>
@@ -31,7 +32,7 @@ const ListadoPacientes = ({pacientes, setPaciente, eliminarPaciente}) => {
                         </span>
                     </p>
                 </>
-            )}
+            )} 
         </div>
     )
 }
